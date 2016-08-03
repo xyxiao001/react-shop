809d9ebfa011ba4c67779920839a8a7d5b562043
xyxiao <463618622@qq.com>
2016-08-03 16:59:51 +0800

    1

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -63,8 +63,46 @@ export default React.createClass({
           jf_price: '250积分'
         }
       ],
-      limits: [],
-      newItems: []
+      limits: [
+        {
+          id: 100,
+          img: 'http://yanxuan.nosdn.127.net/07e2057d35153f063cf042177d6a815e.png?imageView&quality=85&thumbnail=330x330',
+          title: '茶香酥200克',
+          jf_price: '300'
+        },
+        {
+          id: 102,
+          img: 'http://yanxuan.nosdn.127.net/d34867f2e905f76eb7c8f46dcb2e052f.png?imageView&quality=85&thumbnail=330x330',
+          title: '带盖棉麻涤内衣收纳盒',
+          jf_price: '500积分'
+        },
+        {
+          id: 103,
+          img: 'http://yanxuan.nosdn.127.net/4fb112a597732ea093df1d495587b04e.png?imageView&quality=85&thumbnail=330x330',
+          title: '速干防滑沐浴拖鞋',
+          jf_price: '250积分'
+        }
+      ],
+      newItems: [
+        {
+          id: 100,
+          img: 'http://yanxuan.nosdn.127.net/07e2057d35153f063cf042177d6a815e.png?imageView&quality=85&thumbnail=330x330',
+          title: '茶香酥200克',
+          jf_price: '300'
+        },
+        {
+          id: 102,
+          img: 'http://yanxuan.nosdn.127.net/d34867f2e905f76eb7c8f46dcb2e052f.png?imageView&quality=85&thumbnail=330x330',
+          title: '带盖棉麻涤内衣收纳盒',
+          jf_price: '500积分'
+        },
+        {
+          id: 103,
+          img: 'http://yanxuan.nosdn.127.net/4fb112a597732ea093df1d495587b04e.png?imageView&quality=85&thumbnail=330x330',
+          title: '速干防滑沐浴拖鞋',
+          jf_price: '250积分'
+        }
+      ]
     }
   },
   componentDidMount() {
