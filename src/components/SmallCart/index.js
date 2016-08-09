a7493fa3968c01e314537164240f83813e8cc22e
xyxiao <463618622@qq.com>
2016-08-09 12:59:07 +0800

    1

--- src/components/SmallCart/index.js
+++ src/components/SmallCart/index.js
@@ -3,21 +3,21 @@ import { Link } from 'react-router'
 
 export default React.createClass({
   componentDidMount() {
-    var back = document.querySelector('.back')
-    var x1, y1, x2, y2
-    back.addEventListener('touchstart', touchStart, false)
-    back.addEventListener('touchmove', touchMove, false)
-    function touchStart(event) {
-      x2 = event.touches[0].clientX - back.offsetLeft
-      y2 = event.touches[0].clientY - back.offsetTop
-    }
-
-    function touchMove(event) {
-      x1 = event.touches[0].clientX
-      y1 = event.touches[0].clientY
-      back.style.top = y1 - y2 + 'px'
-      back.style.left = x1 - x2 + 'px'
-    }
+    // var back = document.querySelector('.back')
+    // var x1, y1, x2, y2
+    // back.addEventListener('touchstart', touchStart, false)
+    // back.addEventListener('touchmove', touchMove, false)
+    // function touchStart(event) {
+    //   x2 = event.touches[0].clientX - back.offsetLeft
+    //   y2 = event.touches[0].clientY - back.offsetTop
+    // }
+    //
+    // function touchMove(event) {
+    //   x1 = event.touches[0].clientX
+    //   y1 = event.touches[0].clientY
+    //   back.style.top = y1 - y2 + 'px'
+    //   back.style.left = x1 - x2 + 'px'
+    // }
   },
   render() {
     return (
